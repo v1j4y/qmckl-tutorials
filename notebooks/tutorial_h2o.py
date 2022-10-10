8e066d927d71daf86bca65331363ff561b8762ea
v1j4y <vijay.gopal.c@gmail.com>
2022-10-10 17:32:43 +0200

    Update py.

--- notebooks/tutorial_h2o.py
+++ notebooks/tutorial_h2o.py
@@ -64,21 +64,21 @@ end = time.clock_gettime_ns(time.CLOCK_REALTIME)
 
 print(f'Time for the calculation of 1 step : {(end-start)*.000001/ITERMAX} ms')
 
-provide_elec = pq.electron_provided(ctx)
-assert (provide_elec == True)
+provided_elec = pq.electron_provided(ctx)
+assert (provided_elec == True)
 
-provide_nucl = pq.nucleus_provided(ctx)
-assert (provide_nucl == True)
+provided_nucl = pq.nucleus_provided(ctx)
+assert (provided_nucl == True)
 
-provide_ao_basis = pq.ao_basis_provided(ctx)
-assert (provide_ao_basis == True)
+provided_ao_basis = pq.ao_basis_provided(ctx)
+assert (provided_ao_basis == True)
 
-provide_mo_basis = pq.mo_basis_provided(ctx)
-assert (provide_mo_basis == True)
+provided_mo_basis = pq.mo_basis_provided(ctx)
+assert (provided_mo_basis == True)
 
-provide_det = pq.determinant_provided(ctx)
-assert (provide_det == True)
+provided_det = pq.determinant_provided(ctx)
+assert (provided_det == True)
 
-provide_local_energy = pq.local_energy_provided(ctx)
-assert (provide_local_energy == True)
+provided_local_energy = pq.local_energy_provided(ctx)
+assert (provided_local_energy == True)
 #local_energy = pq.get_local_energy(ctx, walk_num)
